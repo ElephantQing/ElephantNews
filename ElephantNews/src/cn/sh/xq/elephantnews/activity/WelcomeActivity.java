72078006f456f4253f169f80d89c3a6454af4913
ElephantQing <xq1983@hotmail.com>
2015-10-01 00:51:08 +0800

    init

---
+++ ElephantNews/src/cn/sh/xq/elephantnews/activity/WelcomeActivity.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package cn.sh.xq.elephantnews.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author HomeStudio
+ * 
+ */
+public class WelcomeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
